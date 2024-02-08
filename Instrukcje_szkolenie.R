8384c62da6d2795c38c8066fc88c68f064539972
BogdanWertz <bogdan.wertz@gmail.com>
2024-02-08 09:25:53 +0100

    Add

---
+++ Instrukcje_szkolenie.R
@@ -0,0 +1,31 @@
+# install.packages("available")
+available::available("bwpakiet", browse = FALSE)
+
+install.packages("usethis")
+library(usethis)
+
+#Utworzenie pakietu
+usethis::create_package("C:/Rtry/bwpakiet")
+
+install.packages("devtools")
+library(devtools)
+devtools::load_all()
+devtools::document()
+devtools::install()
+
+
+konwersja_temp(36)
+?konwersja_temp
+
+
+install.packages("gitcreds")
+gitcreds::gitcreds_set()
+
+#Token 30-dniowy
+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
+
+#Dodanie danych w pakiecie
+moj_df = data.frame(x = 1:10, y = 11:20)
+usethis::use_data(moj_df, overwrite = TRUE)
+
+
